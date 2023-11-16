311d6c73f46f92e5d20faeb8683c7d7876afb0dd
Shreeramm7 <119804206+Shreeramm7@users.noreply.github.com>
2023-11-16 23:19:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let sideMenu = document.getElementById('sidemenu');
+
+function openmenu() {
+  sideMenu.style.right = '0';
+}
+
+function closemenu() {
+  sideMenu.style.right = ' -200px';
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxx0SElcBOlS4xMYd94EQEPPGDMoqZJC0DqKhxDowrkczfi2XeR_E95uIWo8ilSCaL7/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg=document.getElementById('msg');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML="Message sent successfully!"
+      setTimeout(function(){
+        msg.innerHTML=""
+      },3000)
+      form.reset();
+    })
+    .catch((error) => console.error('Error!', error.message));
+});
